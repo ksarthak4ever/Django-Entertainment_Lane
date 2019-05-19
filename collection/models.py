1a8d2268fd45b9cd4cb763104b95c81708735cf4
Sarthak Kumar <ksarthak4ever@gmail.com>
2019-05-19 18:37:04 +0530

    created simple models

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Collection(models.Model):
+	title = models.CharField(max_length=255)
+
+class Video(models.Model):
+	title = models.CharField(max_length=255)
+	url = models.URLField()
+	youtube_id = models.CharField(max_length=255)
+	collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
