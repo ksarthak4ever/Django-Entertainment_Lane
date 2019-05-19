50e8d827279f3106c231352e5fc0d1e1b879ec58
Sarthak Kumar <ksarthak4ever@gmail.com>
2019-05-19 18:27:28 +0530

    added signup,login,logout functionality

--- Entertainment_Lane/urls.py
+++ Entertainment_Lane/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -21,7 +22,10 @@ from collection import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy #used to redirect to homepage when someone signs up sucessfully.
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
 	return render(request, 'collection/home.html')
+
+
+class SignUp(generic.CreateView):
+	form_class = UserCreationForm
+	success_url = reverse_lazy('home')
+	template_name = 'registration/signup.html'
+	
