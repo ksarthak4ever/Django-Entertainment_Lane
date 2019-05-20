ecee73d8bc71b63bfb83b2e5993394d1c7d81519
Sarthak Kumar <ksarthak4ever@gmail.com>
2019-05-20 21:05:04 +0530

    added crud functionality

--- Entertainment_Lane/urls.py
+++ Entertainment_Lane/urls.py
@@ -23,9 +23,14 @@ from collection import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('my_collection/create', views.create_collection.as_view(), name='create_collection'),
+    path('my_collection/<int:pk>/', views.detail_collection.as_view(), name='detail_collection'),
+    path('my_collection/<int:pk>/update', views.update_collection.as_view(), name='update_collection'),
+    path('my_collection/<int:pk>/delete', views.delete_collection.as_view(), name='delete_collection'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- collection/views.py
+++ collection/views.py
@@ -1,15 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy #used to redirect to homepage when someone signs up sucessfully.
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login 
+from .models import Collection
 
 
 def home(request):
 	return render(request, 'collection/home.html')
+
+def dashboard(request):
+	return render(request, 'collection/dashboard.html')
 
 
 class SignUp(generic.CreateView):
 	form_class = UserCreationForm
 	success_url = reverse_lazy('home')
 	template_name = 'registration/signup.html'
+
+	def form_valid(self, form): #to login user as soon as he signsup
+		view = super(SignUp, self).form_valid(form)
+		username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+		user = authenticate(username=username, password=password)
+		login(self.request, user)
+		return view
 	
+
+class create_collection(generic.CreateView):
+	model = Collection
+	fields = ['title']
+	template_name = 'collection/create_collection.html'
+	success_url = reverse_lazy('home')
+
+	def form_valid(self, form):
+		form.instance.user = self.request.user
+		super(create_collection, self).form_valid(form)
+		return redirect('home')
+
+
+class detail_collection(generic.DetailView):
+	model = Collection
+	template_name = 'collection/detail_collection.html'
+
+
+class update_collection(generic.UpdateView):
+	model = Collection
+	template_name = 'collection/update_collection.html'
+	fields = ['title']
+	success_url = reverse_lazy('dashboard')
+
+
+class delete_collection(generic.DeleteView):
+	model = Collection
+	template_name = 'collection/delete_collection.html'
+	success_url = reverse_lazy('dashboard')
+
+
